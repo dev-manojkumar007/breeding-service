8834d1db4dbcce02db48f03908f54f9fc6d1db68
ManojKumar-GL <manoj.kumar@covetrus.com>
2023-02-27 11:58:38 +0530

    Changes Done accroding to Data migrated in the DB. Can Fetch Data from DB

--- src/main/java/com/ideathon/breedingservice/controller/FeatureController.java
+++ src/main/java/com/ideathon/breedingservice/controller/FeatureController.java
@@ -1,12 +1,11 @@
 package com.ideathon.breedingservice.controller;
 
 import com.ideathon.breedingservice.model.Client;
+import com.ideathon.breedingservice.model.Patient;
 import com.ideathon.breedingservice.repo.ClientRepository;
+import com.ideathon.breedingservice.repo.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,16 +15,28 @@ import java.util.List;
 @RequestMapping("/api/feature")
 public class FeatureController {
 
-    @Autowired
     private ClientRepository clientRepository;
+    private PatientRepository patientRepository;
+
+    @Autowired
+    public void setClientRepository(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Autowired
+    public void setPatientRepository(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
 
     @GetMapping("/clients")
     public List<Client> getAllClients() {
         return clientRepository.findAll();
     }
 
-    @PostMapping("/client")
-    public Client addClient(@Validated @RequestBody Client client) {
-        return clientRepository.save(client);
+    @GetMapping("/patients")
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
     }
+
 }
+
